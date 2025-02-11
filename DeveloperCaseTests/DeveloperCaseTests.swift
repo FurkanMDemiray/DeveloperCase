88f47a13a20f10a6b89dfada092ae23e55d3fe8e
FurkanMDemiray <furkanmdemiray@gmail.com>
2025-02-11 18:17:16 +0300

    Initial Commit

---
+++ DeveloperCaseTests/DeveloperCaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  DeveloperCaseTests.swift
+//  DeveloperCaseTests
+//
+//  Created by Melik Demiray on 11.02.2025.
+//
+
+import Testing
+@testable import DeveloperCase
+
+struct DeveloperCaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
